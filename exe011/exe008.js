ccc7192fd578dd11f6659f993285f76d0f67f733
Kysas1 <joao.munnyz@gmail.com>
2024-02-20 17:39:58 -0300

    condição js

---
+++ exe011/exe008.js
@@ -0,0 +1,6 @@
+var vel = 20
+console.log (`A velocidade do seu carro é de ${vel} kmh`)
+if (vel > 60) { //condição simples 
+    console.log ( ' Voce ultrapassou a velocidade. MULTADO !')
+}
+console.log ('Dirija sempre com segurança ')
