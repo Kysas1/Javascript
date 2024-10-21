2234e7cf2ca974d840bb414f5c5dd314b3d0e1d0
Pedro Muniz <joao.munnyz@gmail.com>
2024-10-21 13:47:49 -0300

    reback

--- test/2_inserir/script.js
+++ test/2_inserir/script.js
@@ -1,3 +1,7 @@
-let  a = 1
-a = 'teste 1'
-console.log(a)
+    let listaCores = ['yellow' , 'blue' , 'red' , 'gray' , 'green'];
+    document.body.style.backgroundColor = listaCores[2]; 
+
+    function trocarCores() {
+        document.body.style.backgroundColor = listaCores[Math.floor(5*Math.random())];    
+}
+    setInterval(trocarCores ,1000 )
